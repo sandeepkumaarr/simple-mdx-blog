8b362221d1826372ca7aceafa2b426ebc9ddaee6
Sandeep Kumar <kumar.sandeep648@gmail.com>
2020-05-25 20:32:26 +0530

    added global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,11 +7,27 @@
 // You can delete this file if you're not using it
 
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import Theme from "./src/themes/theme"
+
+const GlobalStyle = createGlobalStyle`
+    
+    * {
+        box-sizing: border-box;
+        margin:0;
+        padding:0;
+    }
+
+    body, html{
+        font-family: ${props => props.theme.fonts.main};
+        height: 100px;
+        background-color: ${props => props.theme.colors.light1};
+    }
+`
 
 export const wrapRootElement = ({ element }) => (
     <ThemeProvider theme={Theme}>
+        <GlobalStyle />
         {element}
     </ThemeProvider>
 )
