227d5d6a92afc8b58bf6dbded80f6ca23c31a314
Sandeep Kumar <kumar.sandeep648@gmail.com>
2020-05-26 11:53:23 +0530

    adding logo to navbar

---
+++ src/components/container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ContainerWrapper } from '../elements'
+import { Nav } from '../components'
+export const Container = ({children}) => {
+
+  
+  return(
+    <ContainerWrapper>
+       <Nav />
+        {children}
+    </ContainerWrapper>
+  )  
+}
